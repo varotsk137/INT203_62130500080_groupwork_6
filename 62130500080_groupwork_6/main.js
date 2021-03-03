0bc430b9158d90af2c989d535ea93ea369282717
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-03-03 10:56:26 +0700

    [Remove/Modify] Remove myprofile.html profile.js and move its' content to index.html and main.js

--- 62130500080_groupwork_6/main.js
+++ 62130500080_groupwork_6/main.js
@@ -1,19 +1,15 @@
 const app = {
     data() {
         return {
-            msg: 'Hello, Vue3'
-            , shown: 1
-            , url: 'https://www.sit.kmutt.ac.th'
+                bg: "./images/bg.jpg"
+            ,   me: "./images/me.jpg"
+            ,   name: "Varot Tirakhuntorn"
+            ,   location: "Thailand"
+            ,   follower: 137
+            ,   projects: 2
+            ,   ranks: 222
         }
     }
-    ,created() {
-        console.log("message is: "+this.msg)
-    }
-    , updated (){
-        console.log("message change to: "+this.msg)
-    }
-    
-
 }
 
 
